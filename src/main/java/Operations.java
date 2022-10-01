dfe00765b25beeb083751d139a30642fed6f98fc
Satyaditya Alok <salok@altimetrik.com>
2022-10-01 14:18:10 +0530

    Add Operation

---
+++ src/main/java/Operations.java
@@ -0,0 +1,14 @@
+public class Operations {
+    public int  add(int a ,int b){
+        return a+b;
+    }
+    public int  addition(int a ,int b){
+        return a+b;
+    }
+    public int  sum(int x ,int y){
+        return x+y;
+    }
+    public int  Total(int m ,int n){
+        return m+n;
+    }
+}
