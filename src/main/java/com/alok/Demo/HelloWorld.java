4ad433eac4f17dbbfc24f92e7b0b671321b013bb
salok <aloks.sqa@gmail.com>
2022-10-04 13:46:27 +0530

    test sonar

--- src/main/java/com/alok/Demo/HelloWorld.java
+++ src/main/java/com/alok/Demo/HelloWorld.java
@@ -2,6 +2,8 @@ package com.alok.Demo;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hellow sonar");
+        System.out.println("Hello sonar");
+        System.out.println("sonar");
+
     }
 }
--- src/main/java/com/alok/Demo/InterviewClass.java
+++
@@ -1,9 +0,0 @@
-package com.alok.Demo;
-
-public class InterviewClass {
-
-    public String display(){
-
-        return "Hello World";
-    }
-}
