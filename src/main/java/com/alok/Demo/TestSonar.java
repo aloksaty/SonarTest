1bbcd83a65e166d7729a3821bde3b179f2b1778b
aloksaty <satyaditya.alok@gmail.com>
2022-10-05 22:45:13 +0530

    Sonar PR

---
+++ src/main/java/com/alok/Demo/TestSonar.java
@@ -0,0 +1,14 @@
+package com.alok.Demo;
+/***
+ *
+ *
+ *
+ * User: alok
+ * Date: 05/10/22
+ *
+ */
+public class TestSonar {
+    public static void main (String[] args) {
+        System.out.println ("Test this PR");
+    }
+}
