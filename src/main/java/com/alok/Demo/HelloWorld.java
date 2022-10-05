466e01c0acf4bdb19a009b2f3194ae3759984a39
aloksaty <satyaditya.alok@gmail.com>
2022-10-05 22:20:42 +0530

    sonar Demo

--- src/main/java/com/alok/Demo/HelloWorld.java
+++ src/main/java/com/alok/Demo/HelloWorld.java
@@ -3,7 +3,9 @@ package com.alok.Demo;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello sonar");
-        System.out.println("sonar");
+        System.out.println("sonarQube");
+
+
 
     }
 }
