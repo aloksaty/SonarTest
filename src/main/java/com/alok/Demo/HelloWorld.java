db6f31032be9e08b668eade7da745879f902e56f
salok <aloks.sqa@gmail.com>
2022-10-05 22:37:16 +0530

    dsdfs

--- src/main/java/com/alok/Demo/HelloWorld.java
+++ src/main/java/com/alok/Demo/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello sonar");
         System.out.println("sonarQube");
+        System.out.println("sonarQube");
 
 
 
