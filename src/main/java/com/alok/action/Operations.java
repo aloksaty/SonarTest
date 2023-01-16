d3645a8e43297d92a1550f0f84173ca19cdb581a
salok <aloks.sqa@gmail.com>
2023-01-16 14:55:27 +0530

    test this

--- src/main/java/com/alok/action/Operations.java
+++ src/main/java/com/alok/action/Operations.java
@@ -27,6 +27,7 @@ public class Operations {
         System.out.println(new Operations().add(12,13));
         System.out.println(new Operations().multiply(12,13));
         System.out.println(new Operations().multiply(14,13));
+        System.out.println(new Operations().multiply(15,13));
 
     }
 }
