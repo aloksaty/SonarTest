610affd9862057483b66b6709525f5db692838c2
salok <aloks.sqa@gmail.com>
2023-01-16 14:33:47 +0530

    adding merge conflict

--- src/main/java/com/alok/action/Operations.java
+++ src/main/java/com/alok/action/Operations.java
@@ -27,6 +27,10 @@ public class Operations {
         System.out.println(new Operations().add(12,13));
         System.out.println(new Operations().multiply(12,13));
         System.out.println(new Operations().multiply(14,13));
+        System.out.println(new Operations().multiply(25,13));
+        System.out.println(new Operations().multiply(25,13));
+        System.out.println(new Operations().multiply(26,13));
+        System.out.println(new Operations().multiply(27,13));
 
     }
 }
