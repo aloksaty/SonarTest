611be723ddf22508c99b0ce41a890ec19e49278d
salok <aloks.sqa@gmail.com>
2023-01-16 19:21:14 +0530

    othr commit

--- src/main/java/com/alok/action/DuplicateCode.java
+++ src/main/java/com/alok/action/DuplicateCode.java
@@ -28,8 +28,8 @@ public class DuplicateCode {
             i++;
         }
         System.out.println("Do some thing");
-        for (int i = 0; i < 10; i++) {
-            System.out.println(i);
+        for (int j = 0; j < 10; j++) {
+            System.out.println(j);
         }
         System.out.println("code from other branch");
         System.out.println("code from master");
